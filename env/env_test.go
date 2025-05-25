0943c93a1b196b26ef8380a7c7fe325c7dac92b8
yottta <andreic9203@gmail.com>
2025-05-25 22:14:56 +0300

    Add expand
    Signed-off-by: yottta <andreic9203@gmail.com>

--- env/env_test.go
+++ env/env_test.go
@@ -5,14 +5,13 @@ import (
 	"testing"
 )
 
-func TestWithoutDefaults(t *testing.T) {
+func TestString(t *testing.T) {
 	t.Run("string with no default", func(t *testing.T) {
 		envs := map[string]string{"envvar": "myval"}
 		setupEnvVars(t, envs)
 		if got, want := String("envvar"), "myval"; got != want {
 			t.Errorf("got a different value than the wanted one. expected: %q; got: %q", want, got)
 		}
-		cleanupEnvVars(t, envs)
 	})
 	t.Run("string with default - env var found", func(t *testing.T) {
 		envs := map[string]string{"envvar": "myval"}
@@ -20,20 +19,21 @@ func TestWithoutDefaults(t *testing.T) {
 		if got, want := StringWithDefault("envvar", "myval2"), "myval"; got != want {
 			t.Errorf("got a different value than the wanted one. expected: %q; got: %q", want, got)
 		}
-		cleanupEnvVars(t, envs)
 	})
 	t.Run("string with default - env var not found", func(t *testing.T) {
 		if got, want := StringWithDefault("envvar", "myval2"), "myval2"; got != want {
 			t.Errorf("got a different value than the wanted one. expected: %q; got: %q", want, got)
 		}
 	})
+}
+
+func TestInt(t *testing.T) {
 	t.Run("int with no default", func(t *testing.T) {
 		envs := map[string]string{"envvar": "1212"}
 		setupEnvVars(t, envs)
 		if got, want := Int("envvar"), 1212; got != want {
 			t.Errorf("got a different value than the wanted one. expected: %q; got: %q", want, got)
 		}
-		cleanupEnvVars(t, envs)
 	})
 	t.Run("int with default - env var found", func(t *testing.T) {
 		envs := map[string]string{"envvar": "1212"}
@@ -41,7 +41,6 @@ func TestWithoutDefaults(t *testing.T) {
 		if got, want := IntWithDefault("envvar", 1111), 1212; got != want {
 			t.Errorf("got a different value than the wanted one. expected: %q; got: %q", want, got)
 		}
-		cleanupEnvVars(t, envs)
 	})
 	t.Run("int with default - env var not int", func(t *testing.T) {
 		envs := map[string]string{"envvar": "121a"}
@@ -49,21 +48,21 @@ func TestWithoutDefaults(t *testing.T) {
 		if got, want := IntWithDefault("envvar", 1111), 1111; got != want {
 			t.Errorf("got a different value than the wanted one. expected: %q; got: %q", want, got)
 		}
-		cleanupEnvVars(t, envs)
 	})
 	t.Run("int with default - env var not found", func(t *testing.T) {
 		if got, want := IntWithDefault("envvar", 1111), 1111; got != want {
 			t.Errorf("got a different value than the wanted one. expected: %q; got: %q", want, got)
 		}
 	})
+}
 
+func TestBool(t *testing.T) {
 	t.Run("bool with no default", func(t *testing.T) {
 		envs := map[string]string{"envvar": "true"}
 		setupEnvVars(t, envs)
 		if got, want := Bool("envvar"), true; got != want {
 			t.Errorf("got a different value than the wanted one. expected: %t; got: %t", want, got)
 		}
-		cleanupEnvVars(t, envs)
 	})
 	t.Run("bool with default - env var found", func(t *testing.T) {
 		envs := map[string]string{"envvar": "false"}
@@ -71,7 +70,6 @@ func TestWithoutDefaults(t *testing.T) {
 		if got, want := BoolWithDefault("envvar", true), false; got != want {
 			t.Errorf("got a different value than the wanted one. expected: %t; got: %t", want, got)
 		}
-		cleanupEnvVars(t, envs)
 	})
 	t.Run("bool with default - env var not bool", func(t *testing.T) {
 		envs := map[string]string{"envvar": "test"}
@@ -79,11 +77,32 @@ func TestWithoutDefaults(t *testing.T) {
 		if got, want := BoolWithDefault("envvar", true), true; got != want {
 			t.Errorf("got a different value than the wanted one. expected: %t; got: %t", want, got)
 		}
-		cleanupEnvVars(t, envs)
 	})
 	t.Run("bool with default - env var not found", func(t *testing.T) {
 		if got, want := BoolWithDefault("envvar", true), true; got != want {
 			t.Errorf("got a different value than the wanted one. expected: %t; got: %t", want, got)
+		}
+	})
+}
+
+func TestExpand(t *testing.T) {
+	t.Run("expand with ref", func(t *testing.T) {
+		envs := map[string]string{
+			"env1": "env1 val",
+			"env2": "env 2 contains also ${env1}",
+		}
+		setupEnvVars(t, envs)
+		if got, want := Expand(String("env2")), "env 2 contains also env1 val"; got != want {
+			t.Errorf("got a different value than the wanted one. expected: %q; got: %q", want, got)
+		}
+	})
+	t.Run("expand with ref on non existing", func(t *testing.T) {
+		envs := map[string]string{
+			"env2": "env 2 contains also ${env1}",
+		}
+		setupEnvVars(t, envs)
+		if got, want := Expand(String("env2")), "env 2 contains also "; got != want {
+			t.Errorf("got a different value than the wanted one. expected: %q; got: %q", want, got)
 		}
 	})
 }
@@ -94,6 +113,9 @@ func setupEnvVars(t *testing.T, in map[string]string) {
 			t.Fatalf("failed to set env %s with val %v", k, v)
 		}
 	}
+	t.Cleanup(func() {
+		cleanupEnvVars(t, in)
+	})
 }
 
 func cleanupEnvVars(t *testing.T, in map[string]string) {
